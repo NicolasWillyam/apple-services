e2f2cdc7a42f6c3a74b5db53deec63a856f4bd72
HoangCoder <93141009+NicolasWillyam@users.noreply.github.com>
2024-07-13 20:21:59 +0700

    Fix all errors

--- apple-services/src/components/container.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Container({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <div className="w-full pt-11">{children}</div>;
-}
